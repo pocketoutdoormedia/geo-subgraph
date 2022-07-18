ff300bc399a972faabd849032241eb9a776f3076
Doug Van Horn <dvanhorn@outsideinc.com>
2022-07-18 15:15:13 -0500

    Attempt to align with prettier.

--- src/tracer.ts
+++ src/tracer.ts
@@ -6,9 +6,7 @@ const ENV = process.env.Environment;
 if (ENV === 'stage' || ENV === 'prod') {
   tracer.init();
   console.log('Datadog tracer configured for DD_ENV: ' + process.env.DD_ENV);
-}
-else
-{
+} else {
   console.log('Datadog tracer is not configured for Environment ' + process.env.Environment);
 }
 
