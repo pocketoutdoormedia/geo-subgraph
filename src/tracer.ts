8cb66823b07cd891777d19c84bc51b1ea5323a06
Doug Van Horn <dvanhorn@outsideinc.com>
2022-07-18 14:49:10 -0500

    Log some data so we know if telemetry is started.

--- src/tracer.ts
+++ src/tracer.ts
@@ -5,6 +5,11 @@ const ENV = process.env.Environment;
 
 if (ENV === 'stage' || ENV === 'prod') {
   tracer.init();
+  console.log('Datadog tracer configured for DD_ENV: ' + process.env.DD_ENV);
+}
+else
+{
+  console.log('Datadog tracer is not configured for Environment ' + process.env.Environment);
 }
 
 export default tracer;
