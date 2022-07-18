a0b9afae4d093aa22c7f5879cedc712465d517fd
Doug Van Horn <dvanhorn@outsideinc.com>
2022-07-18 13:57:46 -0500

    Align with the linter.
    Probably need some kind of local build script that a developer can use
    before committing.

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,8 +1,8 @@
 import tracer from 'dd-trace';
-import { Env } from './config/constants';
 
 // Turn on DataDog APM when DD_ENV is stage or prod.
-let DD_ENV = process.env.DD_ENV;
+const DD_ENV = process.env.DD_ENV;
+
 if (DD_ENV === 'stage' || DD_ENV === 'prod') {
   tracer.init();
 }
